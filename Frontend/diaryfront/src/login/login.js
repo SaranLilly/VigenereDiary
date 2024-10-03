2a839b0ab67b5761dc47d80e398eaa090210e645
Chomchanok99 <115713175+Chomchanok99@users.noreply.github.com>
2024-10-04 01:51:34 +0700

    gg

---
+++ Frontend/diaryfront/src/login/login.js
@@ -0,0 +1,11 @@
+import "./login.css";
+
+function login() {
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+        </div>
+    )
+  }
+  
+  export default login;
