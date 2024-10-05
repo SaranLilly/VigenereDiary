645e31d41110830bdb2b79fca2d5e39ed3542a56
Chomchanok99 <115713175+Chomchanok99@users.noreply.github.com>
2024-10-05 13:22:52 +0700

    g2

--- Frontend/diaryfront/src/App.js
+++ Frontend/diaryfront/src/App.js
@@ -1,11 +1,41 @@
 //import logo from './logo.svg';
 import './App.css';
-//import login from "./components/login";
+//import Login from './components/login/login';
+import './re.js';
+//const login() =><li>Username <span> 555</span></li>
 
 function App() {
+  
+  //const design ={color:"#c16c2b"}
   return (
-    <div className="App">
-      
+    <div className="login-container">
+
+      <div className="login-form">
+        <h2>Vigenere Diary</h2>
+
+        <div className="input-group">
+          <input
+            type="text"
+            placeholder="username"
+          />
+        </div>
+
+        <div className="input-group2">
+          <input
+            type="text"
+            placeholder="password"
+          />
+
+        </div>
+
+        <div className="innerBox">
+          <button>
+            login
+
+          </button>
+        </div>
+
+      </div>
     </div>
   ); 
 }
---
+++ Frontend/diaryfront/src/re.js
@@ -0,0 +1,9 @@
+function re() {
+    return (
+        <div className="login-container">
+            <h2>Login</h2>
+        </div>
+    )
+  }
+  
+  export default re;
